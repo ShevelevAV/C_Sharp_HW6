c148d760c036b4d22a8cf9f8502477c981eca6ad
Aleksandr Shevelev <leksevi@icloud.com>
2022-12-23 20:42:18 +0300

    add task41

---
+++ task41/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3*/
+
+void GetArray(int[] ga)
+{
+    for (int i = 0; i < ga.Length; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        int temp = int.Parse(Console.ReadLine()!);
+        ga[i] = temp;
+    }
+}
+int PosNum(int[] pn)
+{
+    int count = 0;
+    for (int i = 0; i < pn.Length; i++)
+    {
+        if (pn[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество чисел: ");
+int N = int.Parse(Console.ReadLine()!);
+int[] array = new int[N];
+GetArray(array);
+Console.WriteLine();
+int res = PosNum(array);
+Console.WriteLine($"Количество положительных чисел: {res}");
